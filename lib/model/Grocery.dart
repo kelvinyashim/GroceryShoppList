f7bc088ada84c518a20d988c2f683a7f7241024f
KELVIN <kelvinyashim24@gmail.com>
2023-10-27 17:44:55 +0100

    handled exceptions

--- lib/model/Grocery.dart
+++ lib/model/Grocery.dart
@@ -2,13 +2,23 @@ import 'package:shoppinglist/data/categories.dart';
 import 'package:shoppinglist/model/Categories.dart';
 
 class GroceryItem {
- const GroceryItem({ required this.name, required this.quantity, required this.category, required this.id});
+  const GroceryItem(
+      {required this.name,
+      required this.quantity,
+      required this.category,
+      required this.id});
   final String id;
   final String name;
   final int quantity;
   final Category category;
 
- 
-
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'quantity': quantity,
+      'category': category,
+      // add other properties here as needed
+    };
   }
-
+}
